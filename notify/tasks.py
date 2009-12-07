52378d581728f9f46d3ece5312f1c4d0934b6cc2
Ian Lewis <IanMLewis@gmail.com>
2009-12-07 22:29:55 +0900

    celeryはインストールされてない場合、ImproperlyConfiguredエラーを起こす

--- notify/tasks.py
+++ notify/tasks.py
@@ -1,14 +1,20 @@
 # vim:fileencoding=utf8
+from django.core.exceptions import ImproperlyConfigured
+from django.conf import settings
 
-from celery.task import Task
-from celery.registry import tasks
+if 'celery' not in settings.INSTALLED_APPS:
+    raise ImproperlyConfigured("You must add celery to INSTALLED_APPS to use the asyncronous task queue")
 
-from notify import send_notification_now
+try:
+    from celery.task import Task
+    from celery.registry import tasks
 
-class SendNotification(Task):
+    from notify import send_notification_now
 
-    def run(self, users, notice_type, extra_context={}, target_id=None, origin_id=None, ctime=None, exclude_media=[], **kwargs):
-        return send_notification_now(
+    class SendNotification(Task):
+
+        def run(self, users, notice_type, extra_context={}, target_id=None, origin_id=None, ctime=None, exclude_media=[], **kwargs):
+            return send_notification_now(
             users,
             notice_type,
             extra_context=extra_context,
@@ -18,4 +24,6 @@ class SendNotification(Task):
             exclude_media=exclude_media,
         )
 
-tasks.register(SendNotification)
+    tasks.register(SendNotification)
+except ImportError:
+    raise ImproperlyConfigured("You must install celery to use the asyncronous task queue")
