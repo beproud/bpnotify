6c43182898e7fafa40d45720aaaa4449c0bf37a0
Ian Lewis <IanMLewis@gmail.com>
2010-06-09 17:09:05 +0900

    DBバックエンドのmodel 修正

--- notify/models.py
+++ notify/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from commons.model_fields import PickledObjectField, PositiveBigIntegerField
+from commons.model_fields import PickledObjectField
 from account.models import User
 import conf
 
@@ -12,15 +12,15 @@ class Notification(models.Model):
     user = models.ForeignKey(User, verbose_name=_(u'ユーザ'))
     notice_type = models.CharField(_(u'タイプ'), max_length=30, choices=conf.NOTICE_TYPES, db_index=True)
     media = models.CharField(_(u'メディア'), max_length=30, choices=conf.NOTICE_MEDIA, db_index=True)
-    target_id = PositiveBigIntegerField(_(u'Target ID'), blank=True, null=True, db_index=True)
-    origin_id = PositiveBigIntegerField(_(u'Origin ID'), blank=True, null=True, db_index=True)
+    target_id = models.CharField(_(u'Target Id'), max_length=30, null=True, blank=True, db_index=True)
+    origin_id = models.CharField(_(u'Origin Id'), max_length=30, null=True, blank=True, db_index=True)
     ctime = models.DateTimeField(_(u'作成日'), default=datetime.now)
 
     # pickleされたコンテキストデータ
     extra_data = PickledObjectField(editable=False, null=True, blank=True)
 
     @models.permalink
-    def get_view_url(self):
+    def get_absolute_url(self):
         return ('view_notification' (), {
             'id': self.id,
         })
@@ -29,7 +29,6 @@ class Notification(models.Model):
         return "%s (%s, %s)" % (self.user.full_name, self.notice_type, self.media)
 
     class Meta:
-       db_table=u'notifications'
        verbose_name=u'通達'
        verbose_name_plural=u'通知'
        ordering=('-ctime',)
@@ -44,8 +43,5 @@ class NotificationSetting(models.Model):
         return "%s (%s, %s)" % (self.user.full_name, self.notice_type, self.media)
 
     class Meta:
-        db_table = u'notification_settings'
         verbose_name = _(u'通知設定')
         verbose_name_plural = _(u'通知設定')
-
-# TODO: Mediaによってテーブルにコピーする？
