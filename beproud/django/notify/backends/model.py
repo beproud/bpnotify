61efe8a5b4951b2db4e1a7d936d73e92adae724d
Ian Lewis <IanMLewis@gmail.com>
2011-04-22 16:53:06 +0900

    モデルバックエンドのエラー処理の修正

--- beproud/django/notify/backends/model.py
+++ beproud/django/notify/backends/model.py
@@ -5,15 +5,23 @@ from beproud.django.notify.models import Notification
 from django.db import DatabaseError
 
 class NotifyBackend(BaseNotify):
+    """
+    A basic backend that saves to the default
+    Notification model. Extra data must be JSON serializable.
+    """
     def _send(target, notice_type, media, extra_data={}):
+        notification = Notification(
+            notice_type = notice_type,
+            media = media,
+            extra_data = extra_data,
+        )
+        notification.target = target
+
         try:
-            notification = Notification(
-                notice_type = notice_type,
-                media = media,
-                extra_data = extra_data,
-            )
-            notification.target = target
             notification.save()
             return 1
-        except DatabaseError:
+        except (TypeError, DatabaseError):
+            # extra_data could not be serialized to JSON or
+            # there was some kind of Database error
+            # TODO: logging
             return 0
