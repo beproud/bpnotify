c9fcd46203e856bde22ab1fb50b4ff31a9612ca9
Ian Lewis <IanMLewis@gmail.com>
2011-04-22 18:12:40 +0900

    複数のターゲットに送る場合のテストを追加

--- beproud/django/notify/tests/basic.py
+++ beproud/django/notify/tests/basic.py
@@ -1,5 +1,6 @@
 #:coding=utf8:
 
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.test import TestCase
@@ -8,10 +9,11 @@ from beproud.django.notify.tests.base import TestBase
 from beproud.django.notify.models import Notification
 from beproud.django.notify.api import *
 
+
 class BasicNotifyTest(TestBase, TestCase):
     fixtures = ['test_users.json']
     
-    def test_sending(self):
+    def test_sending_model(self):
         user = User.objects.get(pk=2)
         items_sent = notify(user, 'private_msg', extra_data={"spam": "eggs"})
         self.assertEquals(items_sent, 2)
@@ -28,4 +30,55 @@ class BasicNotifyTest(TestBase, TestCase):
         self.assertEquals(news[0].media, 'news')
         self.assertEquals(news[0].notify_type, 'private_msg')
         self.assertEquals(news[0].target, user)
+        self.assertEquals(news[0].extra_data.get('spam'), 'eggs')
+
+    def test_sending_model_multi(self):
+        user = [User.objects.get(pk=1), User.objects.get(pk=2)]
+        items_sent = notify(user, 'private_msg', extra_data={"spam": "eggs"})
+        self.assertEquals(items_sent, 4)
+
+        # User2
+        private_messages = Notification.objects.filter(
+            media='private_messages',
+            target_content_type=ContentType.objects.get_for_model(user[0]),
+            target_object_id=user[0].id,
+        )
+        self.assertEquals(len(private_messages), 1)
+        self.assertEquals(private_messages[0].media, 'private_messages')
+        self.assertEquals(private_messages[0].notify_type, 'private_msg')
+        self.assertEquals(private_messages[0].target, user[0])
+        self.assertEquals(private_messages[0].extra_data.get('spam'), 'eggs')
+
+        news = Notification.objects.filter(
+            media='news',
+            target_content_type=ContentType.objects.get_for_model(user[0]),
+            target_object_id=user[0].id,
+        )
+        self.assertEquals(len(news), 1)
+        self.assertEquals(news[0].media, 'news')
+        self.assertEquals(news[0].notify_type, 'private_msg')
+        self.assertEquals(news[0].target, user[0])
+        self.assertEquals(news[0].extra_data.get('spam'), 'eggs')
+
+        # User2
+        private_messages = Notification.objects.filter(
+            media='private_messages',
+            target_content_type=ContentType.objects.get_for_model(user[1]),
+            target_object_id=user[1].id,
+        )
+        self.assertEquals(len(private_messages), 1)
+        self.assertEquals(private_messages[0].media, 'private_messages')
+        self.assertEquals(private_messages[0].notify_type, 'private_msg')
+        self.assertEquals(private_messages[0].target, user[1])
+        self.assertEquals(private_messages[0].extra_data.get('spam'), 'eggs')
+
+        news = Notification.objects.filter(
+            media='news',
+            target_content_type=ContentType.objects.get_for_model(user[1]),
+            target_object_id=user[1].id,
+        )
+        self.assertEquals(len(news), 1)
+        self.assertEquals(news[0].media, 'news')
+        self.assertEquals(news[0].notify_type, 'private_msg')
+        self.assertEquals(news[0].target, user[1])
         self.assertEquals(news[0].extra_data.get('spam'), 'eggs')
