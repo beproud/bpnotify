5e554c7a0619d728a8bf207da7a57ee42cde3150
Ian Lewis <IanMLewis@gmail.com>
2011-04-25 14:08:10 +0900

    Django管理画面のバグ修正

--- beproud/django/notify/admin.py
+++ beproud/django/notify/admin.py
@@ -2,16 +2,16 @@
 
 from django.contrib import admin
 
-from beproud.django.notify.models import Notification, NoticeSetting
+from beproud.django.notify.models import Notification, NotifySetting 
 
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('target_content_type','target_object_id', 'notice_type','media')
-    list_filter = ('target_content_type', 'notice_type', 'media')
+    list_display = ('target_content_type','target_object_id', 'notify_type','media')
+    list_filter = ('target_content_type', 'notify_type', 'media')
     ordering = ('-ctime',)
 
 class NotifySettingAdmin(admin.ModelAdmin):
-    list_display = ('target','notice_type','media','send')
-    list_filter = ('target_content_type','notice_type', 'media')
+    list_display = ('target','notify_type','media','send')
+    list_filter = ('target_content_type','notify_type', 'media')
     ordering = ('target_content_type','target_object_id')
 
 admin.site.register(Notification, NotificationAdmin)
