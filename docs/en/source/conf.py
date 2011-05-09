a0a4365b1706a38e2022dd6c7a3a40f91038228b
Ian Lewis <IanMLewis@gmail.com>
2011-05-09 19:37:24 +0900

    モジュールインデクスを生成するように修正

--- docs/en/source/conf.py
+++ docs/en/source/conf.py
@@ -139,7 +139,7 @@ html_static_path = ['static']
 #html_additional_pages = {}
 
 # If false, no module index is generated.
-html_use_modindex = False 
+#html_use_modindex = False 
 
 # If false, no index is generated.
 #html_use_index = True
