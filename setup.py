13e700e2a36c3e7b2cef6c70dbe4e06dcb4d6f0f
Ian Lewis <IanMLewis@gmail.com>
2011-06-15 23:24:44 +0900

    0.21 リリース

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
  
 setup (
     name='bpnotify',
-    version='0.2',
+    version='0.21',
     description='Nofication routing for Django',
     author='K.K. BeProud',
     author_email='ian@beproud.jp',
