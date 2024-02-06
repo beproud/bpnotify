d95a46caaf2ea42de61ad79bbbb53d341e57d048
kashewnuts <kashun.nuts@gmail.com>
2024-02-06 10:18:43 +0900

    テスト実行をpytestに移行

--- beproud/django/notify/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from beproud.django.notify.tests.test_basic import *        #NOQA
-from beproud.django.notify.tests.test_mail import *         #NOQA
-from beproud.django.notify.tests.test_storage import *      #NOQA
-from beproud.django.notify.tests.test_tasks import *        #NOQA
